ed410a5c18a48391c3fae46e98af0427fdee5a33
Sharmila0310 <sharmithirunavukkarasu2005@gmail.com>
2026-08-06 22:26:22 +0530

    Delete calculator.py

--- calculator.py
+++
@@ -1,9 +0,0 @@
-# calculator.py
-
-def add(a, b):
-    # Corrected function to add two numbers
-    return a - b
-
-def test_add():
-    # Updated test to reflect the correct behavior of the add function
-    assert add(2, 3) == 5
